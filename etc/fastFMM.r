8de6e8b6bb74aeed108aa6998967014c6e07715d
dylanablack <dylan.black@protonmail.com>
2023-11-23 15:24:06 +1000

    Made some updates to the R script

--- etc/fastFMM.r
+++ etc/fastFMM.r
@@ -1,4 +1,6 @@
 # Load libraries
+# The issue here is that the user will need R with these packages installed
+# I tried with the vignette example but this resulted in some weird hiragana outputs?
 library(lme4)
 library(parallel)
 library(cAIC4)
@@ -18,7 +20,24 @@ library(gridExtra)
 library(Rfast)
 library(fastFMM)
 
-dat <- read.csv("..")
+dat <- read.csv("")
 dat
+
+mod <- fui(Y ~ treatment + # main effect of cue
+              (treatment | id),  # random slope & intercept
+              data = dat,
+              parallel = TRUE,
+              analytic = FALSE) # bootstrap
+
+align_time <- 1 # cue onset is at 2 seconds
+sampling_Hz <- 15 # sampling rate
+# plot titles: interpretation of beta coefficients
+plot_names <- c("Intercept", "Mean Signal Difference: Cue1 - Cue0") 
+fui_plot <- plot_fui(mod, # model fit object
+                     x_rescale = sampling_Hz, # rescale x-axis to sampling rate
+                     align_x = align_time, # align to cue onset
+                     title_names = plot_names,
+                     xlab = "Time (s)",
+                     num_row = 2)
 
 
